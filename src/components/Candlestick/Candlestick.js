7fd4353628e9092e54c2541fabde563add27bdae
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-12-13 17:40:21 -0300

    inputCoin fetch

--- src/components/Candlestick/Candlestick.js
+++ src/components/Candlestick/Candlestick.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import apexcharts from "react-apexcharts"
 import ReactApexChart from "react-apexcharts";
 import styles from './Candlestick.module.css';
 
 class CandleStickChart extends Component {
 
 state = {
+inputCoin:'',
 options: {
     title: {
         text: 'BTC-USD',
@@ -188,11 +188,57 @@ fetch("https://api.coincap.io/v2/candles?exchange=poloniex&interval=d1&baseId=ri
     )
 }
 
+changeHandler = (e)=>{
+        this.setState({ inputCoin: e.target.value });
+}
+
+keySubmit = (e)=>{
+    if (e.keyCode == 13) {
+        console.log('value', e.target.value);
+        fetch("https://api.coincap.io/v2/candles?exchange=poloniex&interval=d1&baseId="+this.state.inputCoin+"&quoteId=tether")
+            .then(res => res.json())
+            .then(
+                (result) => {
+
+                    let coinData = result.data.slice(-90);
+
+                    coinData.forEach(function (d) {
+                        d.open = Math.round(d.open * 100) / 100;
+                        d.high = Math.round(d.high * 100) / 100;;
+                        d.low = Math.round(d.low * 100) / 100;;
+                        d.close = Math.round(d.close * 100) / 100;;
+                    });
+
+                    let candlestickFormat = coinData.map(function (d) {
+                        return {
+                            x: new Date(d.period),
+                            y: [d.open, d.high, d.low, d.close]
+                        }
+                    })
+                    console.log(candlestickFormat);
+                    this.setState({
+                        isLoaded: true,
+                        series: [{ data: candlestickFormat }],
+                        options: { title: { text: this.state.inputCoin + '-USD' } }
+                    });
+                },
+
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+            
+    }
+}
+
 render() {
     return (
         <div>
             <div>
-                <input/>
+                <input value={this.state.inputCoin} onKeyDown={this.keySubmit} onChange={this.changeHandler}/>
             </div>
             <div id="chart" className={styles.CandleStick}>
                 <ReactApexChart options={this.state.options} series={this.state.series} type="candlestick" height="500" />
